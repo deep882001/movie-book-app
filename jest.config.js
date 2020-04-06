50c36f97454190f73298fdb798c92083b977ba7f
Kirit Tanna <kirit.tanna@sapientrazorfish.com>
2020-04-06 14:12:42 -0400

    jest config updates

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
+  moduleNameMapper: {
+    'components(.*)$': '<rootDir>/src/components/$1',
+    '\\.(css|less)$': 'identity-obj-proxy'
+  },
   setupTestFrameworkScriptFile: '<rootDir>/testSetup.js'
 }
